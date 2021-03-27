daff613c1259c6d31ef16466315dc0e64a63e557
= <beckyljones151192@gmail.com>
2021-03-27 14:20:36 +0000

    made pandatrex folder aand taken some lines from birthday

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Birthday from "./components/birthday/birthday";
 import Mickey from "./components/mickey/mickey";
+import PandaTrex from "./components/pandaTrex/pandaTrex";
 
 function App() {
   return (
     <div className="App">
       {/* <Birthday /> */}
-      <Mickey />
+      {/* <Mickey /> */}
+      <PandaTrex />
     </div>
   );
 }
